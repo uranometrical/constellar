5471f1f3212e8393a47ab848d90287aebf257257
Stevie (Tomat) <xxlennygamerxx@gmail.com>
2021-10-03 16:34:21 -0700

    No more panoramas :(

--- src/main/java/io/github/steviegt6/constellar/ConstellarMain.java
+++ src/main/java/io/github/steviegt6/constellar/ConstellarMain.java
@@ -6,16 +6,4 @@ public class ConstellarMain {
     public static final String ClientNameReadable = "Constellar";
     public static final String ClientNameTechnical = "constellar";
     public static final String ClientVersion = "0.1.0-alpha";
-    public static boolean MainMenuLoaded = false;
-    public static ResourceLocation backgroundTexture;
-    public static int panoramaTimer;
-    public static final ResourceLocation[] titlePanoramaPaths = new ResourceLocation[]
-            {
-                    new ResourceLocation("textures/gui/title/background/panorama_0.png"),
-                    new ResourceLocation("textures/gui/title/background/panorama_1.png"),
-                    new ResourceLocation("textures/gui/title/background/panorama_2.png"),
-                    new ResourceLocation("textures/gui/title/background/panorama_3.png"),
-                    new ResourceLocation("textures/gui/title/background/panorama_4.png"),
-                    new ResourceLocation("textures/gui/title/background/panorama_5.png")
-            };
 }
--- src/main/java/io/github/steviegt6/constellar/mixins/gui/GuiMainMenuMixin.java
+++ src/main/java/io/github/steviegt6/constellar/mixins/gui/GuiMainMenuMixin.java
@@ -4,30 +4,15 @@ import io.github.steviegt6.constellar.ConstellarMain;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNoCallback;
-import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiMainMenu.class)
 public abstract class GuiMainMenuMixin extends GuiScreen implements GuiYesNoCallback {
-    @Shadow private ResourceLocation backgroundTexture;
-
-    @Shadow private int panoramaTimer;
-
     @Inject(method = "drawScreen", at = @At("TAIL"))
     public void drawScreen(int p_drawScreen_1_, int p_drawScreen_2_, float p_drawScreen_3_, CallbackInfo ci) {
-        // We need to know when the main menu is loaded to ensure loading of other processes is safe.
-        ConstellarMain.MainMenuLoaded = true;
-        panoramaTimer = ConstellarMain.panoramaTimer;
-
         drawString(fontRendererObj, ConstellarMain.ClientNameReadable + " v" + ConstellarMain.ClientVersion, 2, height - 20, -1);
-    }
-
-    @Inject(method = "initGui", at = @At("TAIL"))
-    public void extractBackground(CallbackInfo ci) {
-        ConstellarMain.backgroundTexture = backgroundTexture;
     }
 }
