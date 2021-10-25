94e07f576840e1d302dcf3c5617dcfee7ffdff7e
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-25 16:46:15 -0700

    aaaaaaaa

--- src/main/java/dev/tomat/constellar/ConstellarMod.java
+++ src/main/java/dev/tomat/constellar/ConstellarMod.java
@@ -3,11 +3,16 @@ package dev.tomat.constellar;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.opengl.Display;
-
-@Mod(name = ConstellarConstants.NAME, modid = ConstellarConstants.ID, version = ConstellarConstants.Version)
+/*
+    replace("@MOD_ID@": project.modId,
+            "@MOD_NAME@": project.modName,
+            "@VERSION@": project.version,
+            "@MOD_ACCEPTED@": project.acceptedMinecraftVersions)
+ */
+@Mod(modid = "@MOD_ID@", name = "@MOD_NAME@", version = "@VERSION@", acceptedMinecraftVersions = "@MOD_ACCEPTED@", useMetadata = true)
 public class ConstellarMod {
     public ConstellarMod() {
         // Incredibly arrogant of us, but I don't really care.
-        Display.setTitle(ConstellarConstants.NAME + " v" + ConstellarConstants.Version + " - " + Minecraft.getMinecraft().getVersion());
+        Display.setTitle("@MOD_NAME@" + " v" + "@VERSION@" + " - " + Minecraft.getMinecraft().getVersion());
     }
 }
