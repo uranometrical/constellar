3fc96d3fa08c11e8456abe41ccd58bb3a0a38022
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-24 23:06:08 -0700

    Fix up mixins + first ported mixin (useless).

---
+++ src/main/java/dev/tomat/constellar/mixin/misc/WindowTitleMixin.java
@@ -0,0 +1,21 @@
+package dev.tomat.constellar.mixin.misc;
+
+import dev.tomat.constellar.ConstellarConstants;
+import net.minecraft.client.Minecraft;
+import net.minecraft.profiler.IPlayerUsage;
+import net.minecraft.util.IThreadListener;
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// Admittedly arrogant of us to change the display.
+// TODO: Injection here is not necessary, ported from standalone client.
+@Mixin(Minecraft.class)
+public abstract class WindowTitleMixin implements IThreadListener, IPlayerUsage {
+    @Inject(method = "createDisplay", at = @At("RETURN"))
+    private void injectWindowTitle(CallbackInfo ci) {
+        Display.setTitle(ConstellarConstants.NAME + " v" + ConstellarConstants.Version);
+    }
+}
