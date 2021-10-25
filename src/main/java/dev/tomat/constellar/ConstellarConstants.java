934b4070ba63d046dca24b4126e8a110cf6aaa2d
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-24 22:11:23 -0700

    a

--- src/main/java/dev/tomat/constellar/ConstellarConstants.java
+++ src/main/java/dev/tomat/constellar/ConstellarConstants.java
@@ -1,6 +1,7 @@
 package dev.tomat.constellar;
 
 public final class ConstellarConstants {
+    public static final String NAME = "Constellar";
     public static final String ID = "constellar";
     public static final String Version = "0.1.0-ALPHA-1";
 }
--- src/main/java/dev/tomat/constellar/ConstellarMod.java
+++ src/main/java/dev/tomat/constellar/ConstellarMod.java
@@ -2,6 +2,6 @@ package dev.tomat.constellar;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = ConstellarConstants.ID, version = ConstellarConstants.Version)
+@Mod(name = ConstellarConstants.NAME, modid = ConstellarConstants.ID, version = ConstellarConstants.Version)
 public class ConstellarMod {
 }
