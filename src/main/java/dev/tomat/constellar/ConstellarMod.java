90e19d479bc5a1e99ffc7c0eca5078cccfc67da4
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-26 18:54:18 -0700

    Some stuff + EULA compliance.

--- src/main/java/dev/tomat/constellar/ConstellarMod.java
+++ src/main/java/dev/tomat/constellar/ConstellarMod.java
@@ -1,8 +1,6 @@
 package dev.tomat.constellar;
 
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
-import org.lwjgl.opengl.Display;
 
 @Mod(
         modid = ConstellarConstants.ID,
@@ -12,8 +10,4 @@ import org.lwjgl.opengl.Display;
         useMetadata = true
 )
 public class ConstellarMod {
-    public ConstellarMod() {
-        // Incredibly arrogant of us, but I don't really care.
-        Display.setTitle(ConstellarConstants.NAME + " v" + ConstellarConstants.VERSION + " - " + Minecraft.getMinecraft().getVersion());
-    }
 }
