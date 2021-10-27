8d11cb3f6ca1d15e512e25812eb89d5b1a4206af
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-26 18:23:02 -0700

    Thinking.

---
+++ src/main/java/dev/tomat/constellar/ConstellarConstants.java
@@ -0,0 +1,8 @@
+package dev.tomat.constellar;
+
+public final class ConstellarConstants {
+    public static final String NAME = "Constellar";
+    public static final String ID = "constellar";
+    public static final String VERSION = "0.1.0-ALPHA-1";
+    public static final String MCVERSION = "1.8.9";
+}
--- src/main/java/dev/tomat/constellar/ConstellarMod.java
+++ src/main/java/dev/tomat/constellar/ConstellarMod.java
@@ -3,16 +3,17 @@ package dev.tomat.constellar;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.opengl.Display;
-/*
-    replace("@MOD_ID@": project.modId,
-            "@MOD_NAME@": project.modName,
-            "@VERSION@": project.version,
-            "@MOD_ACCEPTED@": project.acceptedMinecraftVersions)
- */
-@Mod(modid = "@MOD_ID@", name = "@MOD_NAME@", version = "@VERSION@", acceptedMinecraftVersions = "@MOD_ACCEPTED@", useMetadata = true)
+
+@Mod(
+        modid = ConstellarConstants.ID,
+        name = ConstellarConstants.NAME,
+        version = ConstellarConstants.VERSION,
+        acceptedMinecraftVersions = "[" + ConstellarConstants.MCVERSION + "]",
+        useMetadata = true
+)
 public class ConstellarMod {
     public ConstellarMod() {
         // Incredibly arrogant of us, but I don't really care.
-        Display.setTitle("@MOD_NAME@" + " v" + "@VERSION@" + " - " + Minecraft.getMinecraft().getVersion());
+        Display.setTitle(ConstellarConstants.NAME + " v" + ConstellarConstants.VERSION + " - " + Minecraft.getMinecraft().getVersion());
     }
 }
