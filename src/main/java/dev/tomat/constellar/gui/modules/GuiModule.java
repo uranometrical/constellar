136c9547fecd103a9d4fed27d98e234483cff13d
Steviegt6 <xxlennygamerxx@gmail.com>
2021-11-12 20:38:17 -0800

    Working UI, still needs some improvements (later)

--- src/main/java/dev/tomat/constellar/gui/modules/GuiModule.java
+++ src/main/java/dev/tomat/constellar/gui/modules/GuiModule.java
@@ -6,14 +6,15 @@ import dev.tomat.constellar.utilities.ColorUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 
 public class GuiModule extends GuiButton {
     public static final ResourceLocation Buttons = new ResourceLocation("textures/gui/extra_buttons.png");
     public static final int PADDING = 5;
-    public static final int PADDING_Y = 30;
-    public static final int WIDTH = 75;
-    public static final int HEIGHT = 100;
+    public static final int PADDING_Y = 90;
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 120;
 
     public final IModule Module;
     public int CountX;
@@ -27,11 +28,6 @@ public class GuiModule extends GuiButton {
         CountY = countY;
     }
 
-    public void updatePositions(int width) {
-        xPosition = getX(width, CountX);
-        yPosition = getY(CountY);
-    }
-
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY) {
         if (!visible)
@@ -39,7 +35,11 @@ public class GuiModule extends GuiButton {
 
         GuiUtils.drawRectNormal(xPosition, yPosition, WIDTH, HEIGHT, ColorUtils.colorToInt(50, 50, 50, 100), GuiUtils.PosMode.CENTER);
 
-        ToggleButtonHovered = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;
+        int buttonWidth = WIDTH - 8;
+        int buttonXPos = xPosition - (buttonWidth / 2);
+        int buttonYPos = yPosition + (HEIGHT / 2) - 34;
+
+        ToggleButtonHovered = mouseX >= buttonXPos && mouseY >= buttonYPos && mouseX < buttonXPos + buttonWidth && mouseY < buttonYPos + height;
 
         mc.getTextureManager().bindTexture(Buttons);
 
@@ -49,11 +49,28 @@ public class GuiModule extends GuiButton {
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
         GlStateManager.blendFunc(770, 771);
 
-        drawTexturedModalRect(xPosition, yPosition, 0, 46 + hover * 20, width / 2, height);
-        drawTexturedModalRect(xPosition + width / 2, yPosition, 200 - width / 2, 46 + hover * 20, width / 2, height);
+        drawTexturedModalRect(buttonXPos, buttonYPos, 0, 46 + hover * 20, buttonWidth / 2, height);
+        drawTexturedModalRect(buttonXPos + buttonWidth / 2, buttonYPos, 200 - buttonWidth / 2, 46 + hover * 20, buttonWidth / 2, height);
         mouseDragged(mc, mouseX, mouseY);
 
-        drawCenteredString(mc.fontRendererObj, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, Module.getModuleStatus().getColor());
+        drawCenteredString(
+                mc.fontRendererObj,
+                Module.getModuleStatus().toString(),
+                buttonXPos + buttonWidth / 2,
+                buttonYPos + (height - 8) / 2,
+                Module.getModuleStatus().getColor()
+        );
+
+        int textYPos = yPosition - (HEIGHT / 2);
+
+        drawCenteredString(
+                mc.fontRendererObj,
+                I18n.format(Module.getKey()),
+                buttonXPos + buttonWidth / 2,
+                textYPos + (height - 8) / 2,
+                ColorUtils.colorToInt(255, 255, 255, 255)
+        );
+
     }
 
     @Override
